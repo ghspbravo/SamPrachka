ce6cbce00af3b1ee4cd3304c3b86a87e109e8d58
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2018-09-03 08:54:33 +0500

    improvements

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-window.onload = function () {
+var about = document.getElementById('about');
+var way = document.getElementById('way');
+var equipment = document.getElementById('equipment');
+var metka_about = document.querySelector('.metka_about');
+var metka_way = document.querySelector('.metka_way');
+var metka_equipment = document.querySelector('.metka_equipment');
+metka_about.onclick = function () { scrollToElement(about) }
+metka_way.onclick = function () { scrollToElement(way) }
+metka_equipment.onclick = function () { scrollToElement(equipment) }
 
-	var about = document.getElementById('about');
-	var way = document.getElementById('way');
-	var equipment = document.getElementById('equipment');
-	var metka_about = document.querySelector('.metka_about');
-	var metka_way = document.querySelector('.metka_way');
-	var metka_equipment = document.querySelector('.metka_equipment');
-	metka_about.onclick = function ()
-	{scrollToElement(about)}
-	metka_way.onclick = function ()
-	{scrollToElement(way)}
-	metka_equipment.onclick = function ()
-	{scrollToElement(equipment)}
-}
+window.addEventListener('scroll', function() {
+	Visible (about,metka_about);
+	Visible (way,metka_way);
+	Visible (equipment,metka_equipment);
+});
 
 function scrollToElement(theElement) {
-
-    var selectedPosX = theElement.offsetLeft - 80;
-	var selectedPosY = theElement.offsetTop - 80;
-	
-	console.log(selectedPosX,selectedPosY);
-
-    window.scrollTo(selectedPosX, selectedPosY);
+	var selectedPosY = theElement.offsetTop - 90;
+	window.scrollTo(0, selectedPosY);
 }
 
-
-
+var Visible = function (target,metka) {
+	// Все позиции элемента
+	var targetPosition = {
+		top: window.pageYOffset + target.getBoundingClientRect().top,
+		left: window.pageXOffset + target.getBoundingClientRect().left,
+		right: window.pageXOffset + target.getBoundingClientRect().right,
+		bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+	  },
+	  // Получаем позиции окна
+	  windowPosition = {
+		top: window.pageYOffset,
+		left: window.pageXOffset,
+		right: window.pageXOffset + document.documentElement.clientWidth,
+		bottom: window.pageYOffset + document.documentElement.clientHeight
+	  };
+  
+	if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+	  targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+	  targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+	  targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+	 metka.classList.add("active_scrol");
+	} else {
+	 metka.classList.remove("active_scrol");
+	};
+  };
